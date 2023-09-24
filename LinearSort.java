6e017c0ab93357d8148d03849dbf99604ed7aac5
IAmChetan <100069295+Vishwkarma@users.noreply.github.com>
2023-09-24 23:37:11 +0530

    Add files via upload

---
+++ LinearSort.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class LinearSort {
+
+	public static int linearSearch(int arr[],int x) {
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==x) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+	
+		  
+		        Scanner sr = new Scanner(System.in);
+		        int t = sr.nextInt();
+		        while(t > 0) 
+		        {
+		            int size = sr.nextInt();
+		            int[] input = new int[size];
+		            for (int i = 0; i < size; ++i) 
+		            {
+		                input[i] = sr.nextInt();
+		            }
+		            int val = sr.nextInt();
+		            System.out.println(linearSearch(input,val));
+		            t -= 1;
+		        }
+	}
+
+}
